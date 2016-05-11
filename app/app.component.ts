f45570c4b7bb2a78065654d98c0b04fc4c963fe9
Matt Langston <theblang@gmail.com>
2016-05-10 22:53:23 -0500

    Initial Angular 2 setup

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'b4a',
+    templateUrl: 'app/app.component.html'
+})
+export class AppComponent {
+    title = 'Budget 4 All'
+}
