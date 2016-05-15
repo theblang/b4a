f0cd25979771285e279f8bfe0eeec69849607bb5
Matt Langston <theblang@gmail.com>
2016-05-15 16:18:47 -0500

    Add first component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, Routes, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
 
 @Component({
     selector: 'b4a',
-    templateUrl: 'app/app.component.html'
+    templateUrl: 'app/app.component.html',
+    directives: [ROUTER_DIRECTIVES],
+    providers: [
+        ROUTER_PROVIDERS
+    ]
 })
-export class AppComponent {
+
+@Routes([
+    {
+        path: '/dashboard',
+        component: DashboardComponent
+    }
+])
+
+export class AppComponent implements OnInit {
     title = 'Budget 4 All'
+    
+    constructor(private router: Router) {}
+    
+    ngOnInit() {
+        this.router.navigate(['/dashboard'])
+    }
 }
---
+++ app/dashboard.component.html
@@ -0,0 +1,1 @@
+<h3>This is the dashboard</h3>
