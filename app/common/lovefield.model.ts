9b68519ae2aa11b1a1a55e91ee2138a1b4e9ffd4
Matt Langston <matt@mattblang.com>
2016-09-18 18:59:21 -0500

    Remove old project in preparation for migrating to the CLI

--- app/common/lovefield.model.ts
+++
@@ -1,3 +0,0 @@
-export interface LovefieldModel {
-    toRow(): Object;
-}
--- app/common/query-state.model.ts
+++
@@ -1,6 +0,0 @@
-import * as lf from "lf";
-
-export interface QueryState {
-    query: lf.query.Select,
-    handler: Function
-}
